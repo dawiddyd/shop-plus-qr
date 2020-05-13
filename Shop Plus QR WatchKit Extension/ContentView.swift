f2354393ff757a6406a04dc37d442282d569bd9c
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-05-13 20:39:24 +0200

    Initial Commit

---
+++ Shop Plus QR WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shop Plus QR WatchKit Extension
+//
+//  Created by Dawid Dydliński on 13/05/2020.
+//  Copyright © 2020 Dawid Dydliński. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shop Plus QR WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Shop Plus QR WatchKit Extension
+//
+//  Created by Dawid Dydliński on 13/05/2020.
+//  Copyright © 2020 Dawid Dydliński. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
